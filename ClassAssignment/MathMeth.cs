beff94c26f931c72dfcbd42efbeb7303dfcb852d
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-31 10:05:15 -0700

    Add project files.

---
+++ ClassAssignment/MathMeth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAssignment
+{
+    public class MathMeth
+    {
+        public MathMeth()
+        {
+            int result;
+            result = (num1 + 10);
+            return result;
+        }
+        public int num1 { get; set; }
+    }
+}
---
+++ ClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int b;
+            Console.WriteLine("What number do you want to do math operations on?");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            b = MathMeth(num1);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
